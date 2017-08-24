738bf9a380ebe93c9414291cf10263a920175f53
orzechowskid <orzechod@gmail.com>
2017-08-24 18:03:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,13 +26,21 @@ class ReactElementPostprocessor {
     }
 
     process(value, key, options, translator) {
-        return value.split(this.searchRegex)
-            .filter(Boolean)
+        const tokens = value.split(this.searchRegex);
+
+        if (tokens.length === 1) {
+            /* nothing to interpolate */
+            return tokens[0];
+        }
+
+        return tokens
+            .filter(Boolean) // don't care about empty strings
             .map((token, tokenIndex) => {
                 if (!this.searchRegex.test(token)) {
                     return token;
                 }
 
+                /* strip prefix/suffix from token to get the interpolated variable name */
                 const tokenName = token.replace(this.replaceRegex, `$1`);
                 const element = options[tokenName];
 
@@ -42,10 +50,12 @@ class ReactElementPostprocessor {
                         : ``
                 };
 
+                /* if it's not a React element, don't process it as one */
                 if (!element.$$typeof) {
                     return element;
                 }
 
+                /* clone element and add a `key` prop so React won't complain */
                 return (
                     <element.type
                         {...element.props}
