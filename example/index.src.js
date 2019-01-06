1cf5f97a65bacd1ee36873ae50fea8c682bcb0d4
Dan Orzechowski <orzechod@gmail.com>
2019-01-05 23:57:13 -0500

    modernize

---
+++ example/index.src.js
@@ -0,0 +1,69 @@
+import i18next from 'i18next';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ReactPostprocessor from '../src/index.js';
+
+function t(key, opts) {
+    return i18next.t(key, {
+        ...opts,
+        postProcess: `reactPostprocessor`
+    });
+}
+
+function ExampleComponent() {
+    return (
+        <div>
+            <div>
+                {t(`text`, {
+                    name: `Alice`,
+                    clickHere: (
+                        <span
+                            onClick={() => console.log(`click: Alice`)}
+                            style={{ cursor: `pointer` }}
+                        >
+                            {t(`clickMe`)}
+                        </span>
+                    )
+                })}
+            </div>
+            <div>
+                {t(`text`, {
+                    name: `Bob`,
+                    clickHere: () => (
+                        <span
+                            onClick={() => console.log(`click: Bob`)}
+                            style={{ cursor: `pointer` }}
+                        >
+                            {t(`clickMe`)}
+                        </span>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
+
+i18next
+    .use(new ReactPostprocessor())
+    .init({
+        lng: `en`,
+        resources: {
+            en: {
+                translation: {
+                    clickMe: `click me`,
+                    text: `hello, {{name}}, <clickHere> and you will get a message in your console.`
+                }
+            },
+            fr: {
+                translation: {
+                    clickMe: `cliquez-moi`,
+                    text: `bonjour, {{name}}, <clickHere> et voila.`
+                }
+            }
+        }
+    }, function() {
+        ReactDOM.render((
+            <ExampleComponent />
+        ), document.getElementById(`app`));
+    });
--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,104 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import {
     minify
 } from 'uglify-es';
 
-export default {
+const APP_ENV = process.env.APP_ENV || process.env.NODE_ENV;
+
+const baseConfig = {
     external: [
         `react`
     ],
-    globals: {
-        'react': 'React'
-    },
     input: `src/index.js`,
-    name: `i18next-react-postprocessor`,
     output: {
-        file: `lib/index.js`,
-        format: `umd`
+        name: `i18nextReactPostprocessor`,
+        sourcemap: APP_ENV !== `production`
+            ? `inline`
+            : false
     },
     plugins: [
+        commonjs({
+            include: [
+                /node_modules/,
+                `node_modules/react/**/*`
+            ],
+            sourcemap: false
+        }),
+        nodeResolve({
+            preferBuiltins: true
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
         babel({
             exclude: `node_modules/**`
         }),
-        nodeResolve({}),
-        commonjs({
-            include: 'node_modules/**',
-            sourceMap: false
-        }),
-        uglify({}, minify)
     ]
 };
+
+const umdConfig = {
+    ...baseConfig,
+    output: {
+        ...baseConfig.output,
+        file: `dist/index.js`,
+        format: `umd`,
+        globals: {
+            'react': `React`,
+            'react-dom': `ReactDOM`
+        }
+    },
+    plugins: [
+        ...baseConfig.plugins,
+        ...(APP_ENV === `development`
+            ? []
+            : [ uglify({}, minify) ])
+    ]
+};
+
+const esmConfig = {
+    ...baseConfig,
+    output: {
+        ...baseConfig.output,
+        file: `esm/index.js`,
+        format: `es`
+    },
+    plugins: [
+        ...baseConfig.plugins,
+        ...(APP_ENV === `development`
+            ? []
+            : [ uglify({}, minify) ])
+    ]
+};
+
+const demoConfig = {
+    ...baseConfig,
+    external: [],
+    input: `example/index.src.js`,
+    output: {
+        ...baseConfig.output,
+        file: `example/index.dist.js`,
+        format: `iife`,
+        globals: {},
+        sourcemap: `inline`
+    },
+    //    plugins: [
+    //        commonjs({
+    //            include: [
+    //                /node_modules/,
+    //                `node_modules/react/**/*`
+    //            ],
+    //            sourcemap: false
+    //        }),
+    //        ...baseConfig.plugins
+    //    ]
+};
+
+export default [
+    umdConfig,
+    esmConfig,
+    demoConfig
+];
